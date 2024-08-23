ab0621ec6619678972b5f4c011cef87f104e89d4
Arinji2 <arinjaydhar205@gmail.com>
2024-08-23 16:54:24 +0530

    Add env logger

--- main.go
+++ main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	cronjobs "github.com/Arinji2/sense-backend/cron-jobs"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Sense Backend: Request Received")
 		w.Write([]byte("Sense Backend: Request Received"))
 		render.Status(r, 200)
+	})
+
+	r.Get("/env", func(w http.ResponseWriter, r *http.Request) {
+		keys := os.Environ()
+
+		for _, key := range keys {
+			val := strings.Split(key, "=")
+
+			fmt.Println(val[0] + ": " + val[1])
+		}
+
+		render.Status(r, 404)
 	})
 
 	go startCronJob()
